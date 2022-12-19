88e5e6566dac6e522aa9da54f4dcaf60b67b8587
buidanmo <113153559+buidanmo@users.noreply.github.com>
2022-12-19 16:10:19 +0700

    attorly

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("PRIVATE_PATH", dirname(__FILE__));
+    define("HEADER", PRIVATE_PATH.'/components/header');
+    define("FOOTER", PRIVATE_PATH.'/components/footer');
+    define("TEMPLATES",PRIVATE_PATH.'/components/templates');
+    define("SHORTCODE",PRIVATE_PATH.'/components/shortcode');
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+require ('functions.php');
+include(HEADER.'/header.php');
+include(FOOTER.'/footer.php');
